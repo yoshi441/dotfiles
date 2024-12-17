70b27a9aa60ac5c691601e736c2a62f4cdad0c3a
yoshi441 <yoshi08441@gmail.com>
2024-12-17 12:00:12 +0900

    Rewrite nvim-cmp

---
+++ nvim/lua/plugins/nvim-cmp-new.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = true,
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    lazy = true,
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    lazy = true,
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-cmdline",
+    lazy = true,
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-path",
+    lazy = true,
+    event = "InsertEnter",
+  },
+  {
+    "hrsh7th/cmp-nvim-lua",
+  }
+}
