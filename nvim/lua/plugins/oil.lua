50da4c5b1466216344fcfa2e2731f28b97941fae
yoshi441 <yoshi08441@gmail.com>
2024-12-17 11:58:39 +0900

    Enable lazy loading

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -3,7 +3,9 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = { view_options = { show_hidden = true } },
-  -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+  lazy = true,
+  cmd = {
+    "Oil",
+  }
 }
